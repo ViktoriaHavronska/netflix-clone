e3ea9bf9331458094ae6950cdb89836594eade70
vhavronska <viktoriagavronska1@gmail.com>
2026-03-06 15:49:10 +0100

    Add new feature functionality using JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const rows = document.querySelectorAll('.movie-row');
+const header = document.querySelector('.header');
+const cards = document.querySelectorAll('.movie-card');
+const modal = document.querySelector('.movie-modal');
+const closeModal = document.querySelector('.close-modal');
+const searchInput = document.getElementById("searchInput");
+const movieCards = document.querySelectorAll(".movie-card");
 
 rows.forEach(row => {
     const leftBtn = document.createElement("button");
@@ -25,5 +31,36 @@ rows.forEach(row => {
             left: 400,
             behavior: "smooth"
         });
+    });
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        modal.style.display = "flex";
+    });
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+searchInput.addEventListener("keyup", function () {
+    const searchValue = searchInput.value.toLowerCase();
+    movieCards.forEach(card => {
+        const title = card.getAttribute("data-title").toLowerCase();
+
+        if (title.includes(searchValue)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
     });
 });
